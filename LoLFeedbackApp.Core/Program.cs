1350571754fa9e848e7d05858850be71fa32af98
Ben Bravo <bendc312@gmail.com>
2025-06-13 02:57:33 -0400

    Unit Tests and Window Switch
    Program now launches in a window

--- LoLFeedbackApp.Core/Program.cs
+++ LoLFeedbackApp.Core/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace LoLFeedbackApp.Core
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainForm());
+        }
+    }
+
+    public class MainForm : Form
+    {
+        public MainForm()
+        {
+            this.Text = "LoL Feedback App";
+            this.Size = new System.Drawing.Size(800, 600);
+        }
+    }
+}
